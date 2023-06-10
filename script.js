61be9b1f87789260558df96ea916705085494cb4
bakeungdickson@gmail.com <pierodics@gmail.com>
2023-06-10 08:42:53 +0100

    adjusting and styling contact

--- script.js
+++ script.js
@@ -6,35 +6,20 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 camera.position.z = 5;
 
 // Create a renderer
-// Create a renderer
-const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x000000, 0); // Set the background color to transparent
 document.getElementById('canvas-container').appendChild(renderer.domElement);
-// const renderer = new THREE.WebGLRenderer({ antialias: true });
-// renderer.setSize(window.innerWidth, window.innerHeight);
-// document.getElementById('canvas-container').appendChild(renderer.domElement);
-// Adjust canvas height dynamically
-function adjustCanvasHeight() {
-  const canvasContainer = document.getElementById('canvas-container');
-  const canvasHeight = canvasContainer.offsetHeight;
-  renderer.setSize(window.innerWidth, canvasHeight);
-}
-window.addEventListener('resize', adjustCanvasHeight);
-adjustCanvasHeight();
 
 // Create geometry (icon shape)
-const cubeSize = 2; // Increase the size of the cube
-const geometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.BoxGeometry(1, 1, 1);
 
 // Load textures
 const textureLoader = new THREE.TextureLoader();
-const javascriptTexture = textureLoader.load('Portfolio_Assets/javascript.png');
-const laravelTexture = textureLoader.load('Portfolio_Assets/laravel.png');
-const pythonTexture = textureLoader.load('Portfolio_Assets/python.png');
-const cssTexture = textureLoader.load('Portfolio_Assets/css.png');
-const htmlTexture = textureLoader.load('Portfolio_Assets/html.png');
+const javascriptTexture = textureLoader.load('Assets/javascript.png');
+const laravelTexture = textureLoader.load('Assets/laravel.png');
+const pythonTexture = textureLoader.load('Assets/python.png');
+const cssTexture = textureLoader.load('Assets/css.png');
+const htmlTexture = textureLoader.load('Assets/html.png');
 
 // Create materials using the textures
 const materials = [
